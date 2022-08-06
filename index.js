825d12eb741630e6b80ef4f6261d338de6da27fb
BiancaAlmeida20 <biancaalmeidafarias@gmail.com>
2022-08-05 21:51:15 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const pizzas = [
+    "mussarela",
+    "calabresa", 
+    "portuguesa",
+    "marguerita"
+
+]
+const foundPizza = pizzas.find ( p => p.startswich ( "n"))
+
+// console.log (foundPizza)
+
+do {
+	/* Frutas 
+	***************************************/
+} while (condition);
+
+const fruits = [
+    { nome: "jaca", quantity: 2},
+    { nome: "banana", quantity: 0},
+    { nome: "cereija", quantity: 5},
+]
+
+const foundFruit = fruits.find (fruit => fruit.name = "cereja" )
+
+console.log ( foundFruit )
+
+/*  camiseta
+****************do {
+	***********************/
+ while (condition);
+ 
+ const camiseta = [
+    { nome: "flamengo", quantity: 2},
+    { nome: "corinthians", quantity: 0},
+    { nome: "São Paulo", quantity: 5},
+]
+
+const foundcamiseta = camiseta.find (camiseta => camiseta.name = "cereja" )
+
+console.log ( foundcamiseta )
